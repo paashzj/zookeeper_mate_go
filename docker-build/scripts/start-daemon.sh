d578e1aef9e6595634887104b3d8b4e76bc0ff9e
fu_turer <futurer@outlook.com>
2021-08-17 18:55:49 +0800

    create zk data dir

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+mkdir /opt/sh/zookeeper/data
 nohup $ZOOKEEPER_HOME/mate/zookeeper_mate >$ZOOKEEPER_HOME/zookeeper_mate.log 2>$ZOOKEEPER_HOME/zookeeper_mate_error.log
--- docker-build/scripts/start-zookeeper.sh
+++ docker-build/scripts/start-zookeeper.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-mkdir /opt/sh/zookeeper/data
-
 /bin/bash /opt/sh/zookeeper/bin/zkServer.sh start
