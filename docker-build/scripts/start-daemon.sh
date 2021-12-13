135c76c705157f99802ed1c656dcc5003a1d9ba0
Shoothzj <shoothzj@gmail.com>
2021-12-13 21:11:58 +0800

    Change log path to logs

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 export REMOTE_MODE=false
-mkdir /opt/sh/zookeeper/data
-nohup $ZOOKEEPER_HOME/mate/zookeeper_mate >$ZOOKEEPER_HOME/zookeeper_mate.log 2>$ZOOKEEPER_HOME/zookeeper_mate_error.log
+nohup $ZOOKEEPER_HOME/mate/zookeeper_mate >$ZOOKEEPER_HOME/logs/zookeeper_mate.log 2>$ZOOKEEPER_HOME/logs/zookeeper_mate_error.log
--- docker-build/scripts/start.sh
+++ docker-build/scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+mkdir /opt/sh/zookeeper/data
+mkdir /opt/sh/zookeeper/logs
 $ZOOKEEPER_HOME/mate/config_gen
 bash -x $DIR/start-daemon.sh
 tail -f /dev/null
