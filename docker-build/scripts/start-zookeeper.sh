b8c2957486dca9be321649e60aa5ef13b84634d9
lovehzj <1922919664@qq.com>
2022-01-07 23:05:28 +0800

    correct zookeeper ssl variable

--- docker-build/scripts/start-zookeeper.sh
+++ docker-build/scripts/start-zookeeper.sh
@@ -8,9 +8,9 @@ if [ $ZOOKEEPER_TLS_ENABLE == "true" ]; then
     -Dzookeeper.ssl.keyStore.password=zk_server_pwd
     -Dzookeeper.ssl.trustStore.location=$CERT_DIR/zk_server_trust.jks
     -Dzookeeper.ssl.trustStore.password=zk_server_pwd
-    -Dzookeeper.quorum.keyStore.location=$CERT_DIR/zk_server_key.jks
-    -Dzookeeper.quorum.keyStore.password=zk_server_pwd
-    -Dzookeeper.quorum.trustStore.location=$CERT_DIR/zk_server_trust.jks
-    -Dzookeeper.quorum.trustStore.password=zk_server_pwd"
+    -Dzookeeper.ssl.quorum.keyStore.location=$CERT_DIR/zk_server_key.jks
+    -Dzookeeper.ssl.quorum.keyStore.password=zk_server_pwd
+    -Dzookeeper.ssl.quorum.trustStore.location=$CERT_DIR/zk_server_trust.jks
+    -Dzookeeper.ssl.quorum.trustStore.password=zk_server_pwd"
 fi
 /bin/bash $ZOOKEEPER_HOME/bin/zkServer.sh start
