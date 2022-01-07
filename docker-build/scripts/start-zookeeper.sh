436b0402b04564b42289d5862f4526b989ea9ce5
lovehzj <1922919664@qq.com>
2022-01-07 23:35:39 +0800

    default hostname verification is false

--- docker-build/scripts/start-zookeeper.sh
+++ docker-build/scripts/start-zookeeper.sh
@@ -8,9 +8,12 @@ if [ $ZOOKEEPER_TLS_ENABLE == "true" ]; then
     -Dzookeeper.ssl.keyStore.password=zk_server_pwd
     -Dzookeeper.ssl.trustStore.location=$CERT_DIR/zk_server_trust.jks
     -Dzookeeper.ssl.trustStore.password=zk_server_pwd
+    -Dzookeeper.ssl.hostnameVerification=false
     -Dzookeeper.ssl.quorum.keyStore.location=$CERT_DIR/zk_server_key.jks
     -Dzookeeper.ssl.quorum.keyStore.password=zk_server_pwd
     -Dzookeeper.ssl.quorum.trustStore.location=$CERT_DIR/zk_server_trust.jks
-    -Dzookeeper.ssl.quorum.trustStore.password=zk_server_pwd"
+    -Dzookeeper.ssl.quorum.trustStore.password=zk_server_pwd
+    -Dzookeeper.ssl.quorum.hostnameVerification=false
+    "
 fi
 /bin/bash $ZOOKEEPER_HOME/bin/zkServer.sh start
