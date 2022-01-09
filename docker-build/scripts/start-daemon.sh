a15039c21b7b7855337616ed8c9727284494abd2
lovehzj <1922919664@qq.com>
2022-01-09 15:02:42 +0800

    fix mate log path

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 export REMOTE_MODE=false
-nohup $ZOOKEEPER_HOME/mate/zookeeper_mate >>$ZOOKEEPER_HOME/logs/zookeeper_mate.stdout.log 2>$ZOOKEEPER_HOME/logs/zookeeper_mate.stderr.log
+mkdir $ZOOKEEPER_HOME/data
+mkdir $ZOOKEEPER_HOME/logs
+$ZOOKEEPER_HOME/mate/config_gen
+nohup $ZOOKEEPER_HOME/mate/zookeeper_mate >>$ZOOKEEPER_HOME/logs/zookeeper_mate.stdout.log 2>>$ZOOKEEPER_HOME/logs/zookeeper_mate.stderr.log
+
--- docker-build/scripts/start.sh
+++ docker-build/scripts/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
-mkdir /opt/sh/zookeeper/data
-mkdir /opt/sh/zookeeper/logs
-$ZOOKEEPER_HOME/mate/config_gen
 bash -x $DIR/start-daemon.sh
 tail -f /dev/null
