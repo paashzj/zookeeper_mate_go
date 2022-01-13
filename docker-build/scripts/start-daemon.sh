f2a7742f397a8988b9d37a9aec1a1db49e9b09a4
lovehzj <1922919664@qq.com>
2022-01-13 23:34:25 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -2,7 +2,7 @@
 
 export REMOTE_MODE=false
 mkdir $ZOOKEEPER_HOME/data
-mkdir $ZOOKEEPER_HOME/logs
+mkdir -p $ZOOKEEPER_HOME/logs
 $ZOOKEEPER_HOME/mate/config_gen
 nohup $ZOOKEEPER_HOME/mate/zookeeper_mate >>$ZOOKEEPER_HOME/logs/zookeeper_mate.stdout.log 2>>$ZOOKEEPER_HOME/logs/zookeeper_mate.stderr.log
 
